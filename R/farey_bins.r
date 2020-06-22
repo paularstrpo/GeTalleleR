045cefb8717a78c4ddb50945ea1b50eb73509cc4
paularstrpo <paularstrpo@gmail.com>
2020-06-22 12:30:04 -0400

    update farey_bins to return correctly

--- R/farey_bins.r
+++ R/farey_bins.r
@@ -1,28 +1,33 @@
+#' Create bins with a farey sequence
+#' @param n A number.
+#' @return A list with the bin edges and bin centres for the resulting sequence.
 farey_bins <- function(n){
-
+    # original matlab code:
     # bin_centres=farey_sequence(n);
 
-    # for_edges = bin_centres(:)';
+    # bin_edges = bin_centres(:)';
 
-    # binwidth = diff(for_edges);
-    # for_edges = [0, for_edges(1:end-1)+binwidth/2, 1];
-    # for_edges = full(real(for_edges));
+    # binwidth = diff(bin_edges);
+    # bin_edges = [0, bin_edges(1:end-1)+binwidth/2, 1];
+    # bin_edges = full(real(bin_edges));
 
     # % Shift bins so the interval is ( ] instead of [ ) for
 
-    # bin_edges = for_edges + eps(for_edges);
+    # bin_edges = bin_edges + eps(bin_edges);
     # bin_edges(1) = 0;
     # bin_edges(end) = 1;
 
+    # Use the farey() function from the elliptic package
     bin_centres <- elliptic::farey(n)
-    for_edges <- t(bin_centres)
-    binwidth <- diff(for_edges)
-    for_edges <- c(0, for_edges[c(1:end-1)]+binwidth/2, 1)
-    for_edges <- as.matrix(as.numeric(for_edges))
+    bin_edges <- as.numeric(t(bin_centres))
+    binwidth <- as.numeric(diff(bin_edges))
+
+    bin_edges <- c(0, bin_edges[-1]+binwidth/2, 1)
+    bin_edges <- as.matrix(as.numeric(bin_edges))
 
     # Shift bins so the interval is ( ] instead of [ ) 
-    bin_edges <- for_edges + 0L
+    bin_edges <- bin_edges + 0L
     bin_edges[c(1, length(bin_edges))] <- c(0, 1)
 
-    return(bin_edges, bin_centres)
+    return(list(edges=bin_edges, centres=bin_centres))
 }
