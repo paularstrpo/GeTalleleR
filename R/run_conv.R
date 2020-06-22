21fa6472a227a59293e34ad4c1fb5ee543186fc9
paularstrpo <paularstrpo@gmail.com>
2020-06-22 14:32:25 -0400

    update gitignore

--- R/run_conv.R
+++
@@ -1,9 +0,0 @@
-library("matconv")
-flist <- list.files('GeTallele/toolbox', '*.m$')
-
-newflist <- paste0('R/', gsub('.m', '.r', flist, fixed=TRUE))
-
-sapply(flist, function(x){
-    mat2r(inMat=paste0('GeTallele/toolbox/', x), pathOutR =  paste0('R/', gsub('.m', '.r', x, fixed=TRUE)))
-})
-# note: this package resulted in a pretty awful translation! :(
