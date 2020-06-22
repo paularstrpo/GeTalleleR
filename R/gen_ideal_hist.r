523127a6e1820658daa7a2c2df39325d3d78f6e8
paularstrpo <paularstrpo@gmail.com>
2020-06-22 14:23:36 -0400

    some syntax proofreading & format definitions

--- R/gen_ideal_hist.r
+++ R/gen_ideal_hist.r
@@ -1,14 +1,30 @@
-gen_ideal_hist <- function(matrix,bin_edges){
+gen_ideal_hist <- function(matrix, bin_edges, layer_map){
 
-# comment
+# Demand that the input matrix be a data frame with the following format:
+# CHR | POS | REF | ALT | Ttr | Ntr | Nex | Tex
 
-for (layer in 4:-1:1){
+# Layer map must be a named character map column names to layer type. 
+# Example:
+#                              Ttr                              Ntr 
+# "X013_Ttr_BRCA_TCGA.BH.A0B8.01A" "X013_Ntr_BRCA_TCGA.BH.A0B8.11A" 
+#                              Nex                              Tex 
+# "X013_Nex_BRCA_TCGA.BH.A0B8.11A" "X013_Tex_BRCA_TCGA.BH.A0B8.01A" 
 
+# extract chromosome
+chr <- matrix[, 1]
+
+for (layer in layer_map){
+    
+    layer <- matrix[, layer]
+
+    # is this grabbing the column with tumor / normal? confused about the original input format
+    
     l1 <- layer+6
     l2 <- l1+4
 
-    idx_c=find(matrix(:,1)<=22)
-    counts_vector <- matrix(idx_c,l1)+matrix(idx_c,l2)
+
+    idx_c <- which(chr<=22)
+    counts_vector <- matrix[idx_c, l1] + matrix[idx_c,l2]
 
     model_samples <- struct('smpl', cell(1,51))
 
@@ -16,17 +32,17 @@ for (layer in 4:-1:1){
         nb_reads <- numel(counts_vector)
 
         p <- k/100
-        p <- p*ones(1,5000)
+        p <- p*rep(1, 5000)
         new_count_idx <- randi(nb_reads,1,5000)
         new_count <- counts_vector(new_count_idx)
-        y <- binornd(new_count(:),p(:))
+        y <- rbinom(new_count(:),p(:))
         surr1 <- y(:)./new_count(:)
 
         p <- 1-k/100
         p <- p*ones(1,5000)
         new_count_idx <- randi(nb_reads,1,5000)
         new_count <- counts_vector(new_count_idx)
-        y <- binornd(new_count(:),p(:))
+        y <- rbinom(new_count(:),p(:))
         surr2 <- y(:)./new_count(:)
 
         model_samples(k-49).smpl <- [surr1; surr2]
